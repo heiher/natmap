74ecfee8f1d32b7bd9ca89810977a6394ecdd1df
hev <r@hev.cc>
2025-02-17 12:25:33 +0800

    HevExec: Fix fork and exec on Windows.

--- src/hev-exec.h
+++ src/hev-exec.h
@@ -2,13 +2,26 @@
  ============================================================================
  Name        : hev-exec.h
  Author      : hev <r@hev.cc>
- Copyright   : Copyright (c) 2022 xyz
+ Copyright   : Copyright (c) 2022 - 2025 xyz
  Description : Exec
  ============================================================================
  */
 
 #ifndef __HEV_EXEC_H__
 #define __HEV_EXEC_H__
+
+#ifdef __MSYS__
+#define HEV_EXEC_STACK_SIZE (16384)
+#else
+#define HEV_EXEC_STACK_SIZE (1)
+#endif
+
+/**
+ * hev_exec_init:
+ *
+ * Initialize exec.
+ */
+void hev_exec_init (void *stack);
 
 /**
  * hev_exec_run:
